23cff5dd0eec1165f3ffbad1e9e9bf59b792a88c
Lindsey <linlidiaramirez@gmail.com>
2023-02-20 20:44:01 -0500

    added static pgs as test

--- src/pages/blogpage2.js
+++ src/pages/blogpage2.js
@@ -16,7 +16,7 @@ export default function about(){
             <h1>Blog Publish date: {cMonth}/{cDay}/{cYear}</h1>
         </div>    
             <div>
-                <Image alt="" src="https://getwallpapers.com/wallpaper/full/1/0/c/1344537-kitty-cat-wallpaper-1920x1280-meizu.jpg"/>
+                <Image alt="" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2019%2F09%2F415505-baby-grumpy-cat-kitten.jpg&f=1&nofb=1&ipt=6c0496f661d6343a5e782ed201a74c1bd77e9527592986ddcab9fa0d029b31e7&ipo=images"/>
                 <p> you serious?!! furrildooo? Text and stories aquiiiiii</p>
             </div>
         </div>
