1465a4c8adf02e220a9ce974f5d5f5b0818bd8d9
Lindsey <linlidiaramirez@gmail.com>
2023-02-20 21:07:59 -0500

    added test static blg pgs

--- src/pages/blogpage2.js
+++ src/pages/blogpage2.js
@@ -18,7 +18,7 @@ export default function about(){
             <div>
                 <Image alt="" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2019%2F09%2F415505-baby-grumpy-cat-kitten.jpg&f=1&nofb=1&ipt=6c0496f661d6343a5e782ed201a74c1bd77e9527592986ddcab9fa0d029b31e7&ipo=images"/>
                 <p> you serious?!! furrildooo? Text and stories aquiiiiii</p>
-            </div>
+            </div>   
         </div>
     )
 }
