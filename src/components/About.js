7fffa584fb0c88d9ca0bc6afc86a7b138df77efd
Lindsey <linlidiaramirez@gmail.com>
2023-03-12 03:01:02 -0400

    rmved uneeded styles in about

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
     return(
         <section id="about">
         <div className="container mx-auto flex flex-col-reverse pt-14 p-4 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <div className="md:w-1/2 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             
               <div class="flex font-link-bold flex-col">
               <ul class="text-4xl text-white font-semibold pt-6 pb-3 md:pt-0">Hola, soy</ul>
